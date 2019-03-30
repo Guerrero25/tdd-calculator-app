6705739c1900004364eebf17a12f960e73c8a6de
Omar Guerrero <guerrero9725@gmail.com>
2019-03-30 09:56:39 -0500

    refactor: Setup folder

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+/* Styles */
+import "../../assets/styles/main.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">App</div>;
+  }
+}
+
+export default App;
